825161f8975bffb909963a837a18ff205821d854
jinho <jinho0547@naver.com>
2023-09-07 14:16:18 +0900

    asdasd
    adasd

--- src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
+++ src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class JavaSort <T extends Comparable<T>> implements Sort<T>{
     @Override
-    public List<T> sort(List<T> list) { 
+    public List<T> sort(List<T> list) {
         List<T> output = new ArrayList<>(list);
         Collections.sort(output);
         return output;
